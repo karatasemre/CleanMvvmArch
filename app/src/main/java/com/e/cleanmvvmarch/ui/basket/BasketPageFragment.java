38d87c4ed9d65bf0f307252c8469d80f379a3322
09370190 <emre_karatas@kocsistem.com.tr>
2019-09-19 10:04:29 +0300

    product detail added

---
+++ app/src/main/java/com/e/cleanmvvmarch/ui/basket/BasketPageFragment.java
@@ -0,0 +1,10 @@
+package com.e.cleanmvvmarch.ui.basket;
+
+import com.e.cleanmvvmarch.base.BaseFragment;
+
+public class BasketPageFragment extends BaseFragment {
+    @Override
+    protected int layoutRes() {
+        return 0;
+    }
+}
